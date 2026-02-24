05ade259cc4a32ee564b2ee95e1d0e03dbee3785
Bilal Malik <outlookbilalmalik@gmail.com>
2026-02-24 23:55:55 +0500

    Updated with ts of vission

--- src/app/component/ourvision/ourvision.component.ts
+++ src/app/component/ourvision/ourvision.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
@@ -10,6 +10,28 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
   templateUrl: './ourvision.component.html',
   styleUrl: './ourvision.component.css'
 })
-export class OurvisionComponent {
+export class OurvisionComponent implements AfterViewInit {
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    const elements = this.el.nativeElement.querySelectorAll('.scroll-animate');
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(
+              'animate__animated',
+              'animate__fadeInUp',
+            );
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.2 },
+    );
+
+    elements.forEach((el: Element) => observer.observe(el));
+  }
 
 }
